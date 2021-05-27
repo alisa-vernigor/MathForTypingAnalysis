224fe0884cdbb6c9208e33a83be060aab51443f4
Alice Vernigor <55710346+alisa-vernigor@users.noreply.github.com>
2021-05-27 10:33:17 +0300

    Update tests (WIP)

--- Tests/tests.h
+++ Tests/tests.h
@@ -4,8 +4,15 @@
 #include "MathModule.h"
 #include "Functions/Function.h"
 #include "ParallelModule.h"
+#include "TimeMeasurer.h"
 
 class Tests {
+    using Result = struct {
+                        std::chrono::microseconds max_time;
+                        std::chrono::microseconds min_time;
+                        std::chrono::microseconds average_time;
+                   };
+
 public:
     Tests(size_t size);
 
@@ -38,15 +45,46 @@ public:
     void test_density_default_default(std::vector<double>& means,
                                       std::vector<double>& grid,
                                       std::vector<double>* result);
-private:
-    void init_x(size_t size, std::vector<double>& x, double step);
+    template<typename TFunc>
+    Result test_one(TFunc test, std::vector<double>& x,
+                  size_t it_num, size_t skip_num = 0) {
+        using namespace std::chrono;
 
-    void init_m(size_t size);
+        std::vector<double> res;
+        Result measures;
+        microseconds time;
+
+        for (size_t i = 0; i < it_num; ++i) {
+            if (i >= skip_num) {
+                CTimeAnchor c;
+                test(m_, x, &res);
+                time = c.get_time();
+
+                if (i == skip_num) {
+                    measures.average_time = measures.max_time = measures.min_time = time;
+                } else {
+                    measures.average_time += time;
+                    if (measures.max_time < time) {
+                        measures.max_time = time;
+                    }
+                    if (measures.min_time > time) {
+                        measures.min_time = time;
+                    }
+                }
+            }
+        }
+        measures.average_time /= it_num - skip_num;
+        return measures;
+    }
 
     std::vector<double> m_;
     std::vector<double> x1_;
     std::vector<double> x2_;
     std::vector<double> x3_;
+private:
+    void init_x(size_t size, std::vector<double>& x, double step);
+
+    void init_m(size_t size);
 };
 
 #endif // TESTS_H
--- TimeMeasurer.h
+++ TimeMeasurer.h
@@ -8,17 +8,9 @@ class CTimeAnchor {
   using steady_clock = std::chrono::steady_clock;
 
 public:
-  CTimeAnchor(const char*);
-  ~CTimeAnchor();
-
+  std::chrono::microseconds get_time();
 private:
   steady_clock::time_point begin_ = steady_clock::now();
-  std::string message_;
-};
-
-class CTimeMeasurer {
-public:
-  static CTimeAnchor count(const char*);
 };
 
 #endif // TIMEMEASURER_H
